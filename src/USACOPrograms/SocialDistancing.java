b43d261290e97b39efc884710f34ed89cf0278f6
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2025-01-12 10:00:32 -0800

    Clarified inputs and outputs for SocialDistancing.java

--- src/USACOPrograms/SocialDistancing.java
+++ src/USACOPrograms/SocialDistancing.java
@@ -3,6 +3,15 @@ import java.io.*;
 import java.util.TreeMap;
 
 //USACO 2020 US Open Contest, Silver (9 out of 10 passed)
+// Question - https://usaco.org/index.php?page=viewproblem2&cpid=1038
+/*
+INPUT FORMAT (file socdist.in):
+The first line of input contains N and M. The next M lines each describe an interval in terms of two integers a and b, where 0≤a≤b≤1018. 
+No two intervals overlap or touch at their endpoints. A cow standing on the endpoint of an interval counts as standing on grass.
+
+OUTPUT FORMAT (file socdist.out):
+Print the largest possible value of D such that all pairs of cows are D units apart. A solution with D>0 is guaranteed to exist.
+*/
 
 public class SocialDistancing{
     public static void main(String[] args) throws IOException{
