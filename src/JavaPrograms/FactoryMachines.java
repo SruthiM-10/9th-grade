a0937a7b518701adcfaf660f4a90901801c0b274
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2025-01-12 09:32:11 -0800

    Update FactoryMachines.java
    Clarified inputs and outputs.

--- src/JavaPrograms/FactoryMachines.java
+++ src/JavaPrograms/FactoryMachines.java
@@ -2,6 +2,16 @@ package JavaPrograms;
 import java.io.*;
 
 //(CSES) USACO Guide Silver Section "Binary Search"
+/*
+A factory has n machines which can be used to make products. Your goal is to make a total of t products.
+For each machine, you know the number of seconds it needs to make a single product. The machines can work simultaneously, and you can freely decide their schedule.
+What is the shortest time needed to make t products?
+Input
+The first input line has two integers n and t: the number of machines and products.
+The next line has n integers k_1,k_2,...,k_n: the time needed to make a product using each machine.
+Output
+Print one integer: the minimum time needed to make t products.
+*/
 
 public class FactoryMachines {
     public static void main(String[] args) throws IOException {
